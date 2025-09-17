352406d5b07b714b44f91df49664b455d3a077c7
Andreigrd2004 <andreigrd2@gmail.com>
2025-09-17 23:18:13 +0300

    Adding the user schema model

---
+++ models/user.model.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+        minLength:2,
+        maxLength:50,
+    },
+
+    email:{
+        type:String,
+        required:[true, 'Please enter an email, is required'],
+        unique:true,
+        trim:true,
+        lowercase:true,
+        match:[/\S+@\S+\.\S+/, 'Please enter a valid email address'],
+
+    },
+
+    password:{
+        type:String,
+        required:[true, 'Please enter a password'],
+        minLength:6,
+    }
+
+}, { timestamps: true});
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
