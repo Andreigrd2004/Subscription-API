77f076a24d7fc747b7472107445f1f037fda7ca5
Andreigrd2004 <andreigrd2@gmail.com>
2025-09-17 23:07:01 +0300

    modifying function from declaration from arrow function to normal function in order to make 'this' work in this context

--- models/subscription.model.js
+++ models/subscription.model.js
@@ -68,7 +68,7 @@ const subscriptionSchema = new mongoose.Schema({
 }, {timestamps: true});
 
 
-subscriptionSchema.pre("save", async (next) => {
+subscriptionSchema.pre("save", function (next) {
     if(!this.renewalDate){
         const renewalPeriods = {
             daily: 1,
